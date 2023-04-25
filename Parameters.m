4e96617a9d18e2ff1128f6fed052358930b813fc
efe.seker <efe.seker@inores.com>
2023-04-25 15:43:01 +0300

    Initial commit

---
+++ Parameters.m
@@ -0,0 +1,14 @@
+clear;clc;
+
+Time = 200; % sec
+Td = 1; % Step time
+
+current = 10; % Amp
+Energy_const = 1.3;
+power_const = .323;
+limit_power = 1000;
+
+% Calibratables
+a_const = .242; % constant
+a_const_e = 0;
+a_const_o = 1;
